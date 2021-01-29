c81e1c4d2fde28142c380d9fe6195d9fe1c0a921
Jacob Klimenko <droidedjake@gmail.com>
2021-01-29 18:08:52 -0500

    commit tree f725d3818a7b6d6e4d89c29013d145fb88bd08ca

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id].to_i)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id].to_i)
+
+    erb :edit
+  end
+
+  post '/articles' do
+    @article = Article.find_or_create_by(title: params[:title], content: params[:content])
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id].to_i)
+    @article.title = "#{params[:title]}"
+    @article.content = "#{params[:content]}"
+    @article.save
+
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by_id(params[:id].to_i)
+    @article.destroy
+
+    redirect "/articles"
+  end
+
 end
